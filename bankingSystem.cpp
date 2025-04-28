b8eb155110fcdd714eb6407c9ce2b534ab7ecbd5
joel thomas <joel.thomas.1701@gmail.com>
2025-04-28 19:53:23 +0530

    code2

--- bankingSystem.cpp
+++ bankingSystem.cpp
@@ -11,7 +11,7 @@ class node{ //node class is used for storing transation history
             node* next;
 
             node(float ammount, string type){
-                this->ammount=ammount;
+                this->ammount=ammount; 
                 this->type=type;
                 next=NULL;
             }
@@ -20,9 +20,29 @@ class node{ //node class is used for storing transation history
 class BankAcc{
     private:
             int accountNo; //account number of user
-            string name; //
+            string name; // name of the user
+            float balance;
+            node* head; //head of the linked list that we used to store transaction history
+    public:
+            BankAcc(){ //constructor for the class BankAcc
+                balance=0; 
+                head=NULL;
 
-}
+            }
+
+            void MakeAnAccount(){
+                cout<<"Enter Account Number"<<"\n";
+                cin>>accountNo;
+                cout<<"Enter Your Name"<<"\n";
+                cin>>name;
+                cout<<"Enter the ammount of money you want to deposit initially"<<"\n";
+                cin>>balance;
+
+            }
+
+            void 
+
+};
 int main(){
   
     
