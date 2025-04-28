31b65c5c32bfba4346a349afe2907e4ecaa49a35
joel thomas <joel.thomas.1701@gmail.com>
2025-04-28 19:30:19 +0530

    code1

--- bankingSystem.cpp
+++ bankingSystem.cpp
@@ -1,6 +1,29 @@
+
 #include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
+
+class node{ //node class is used for storing transation history
+    public:
+            float ammount;
+            string type;
+            node* next;
+
+            node(float ammount, string type){
+                this->ammount=ammount;
+                this->type=type;
+                next=NULL;
+            }
+};
+
+class BankAcc{
+    private:
+            int accountNo; //account number of user
+            string name; //
+
+}
 int main(){
-   cout<<"hi";
+  
     
 }
