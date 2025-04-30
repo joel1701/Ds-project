9883790af600fbfc53405114ee7c10cfa1562640
KrishnaTanay0607 <krishnatanay0607@gmail.com>
2025-04-30 20:36:21 +0530

    Added my part of the assignment

--- bankingSystem.cpp
+++ bankingSystem.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 class node{ //node class is used for storing transation history
     public:
+    cout<<"hi im krishna";
             float ammount;
             string type;
             node* next;
